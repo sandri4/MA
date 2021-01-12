37b85b019ecdbdcb703336080e34239d194492be
sandri4 <pletin1997@outlook.com>
2021-01-12 21:49:06 +0200

    add scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :images, as: :imageable
+
+  scope :published, -> { where.not(published_at: nil) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
+  EMAIL_VALID = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   has_many :posts
   has_many :images, as: :imageable
 
-  EMAIL_VALID = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  scope :adults, -> { where('birthday <= ?', 18.years.ago) }
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 8, maximum: 63 }
-  validates :email, format: { with: EMAIL_VALID }
+  validates :email, presence: true, format: { with: EMAIL_VALID }, uniqueness: true
 
   def full_name
     "#{first_name} #{last_name} "
